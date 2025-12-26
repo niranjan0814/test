0d2f6f4784355e7a97f00db5f746d7f3851e13cb
niranjan0814 <niranjansivarajah35@gmail.com>
2025-12-26 20:31:10 +0530

    sprint one overall

--- fincore-app/services/api.config.ts
+++ fincore-app/services/api.config.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000/api';
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'https://teste.agroplus.lk/api';
 
 export const getHeaders = () => {
     // Always get the latest token from storage
